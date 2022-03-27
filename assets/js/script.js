206d084921c96652a5b54600d4678903c447650d
ryanmuhl <ryanmuhl@hotmail.com>
2022-03-27 10:14:45 -0400

    update current weather info section of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var cityContainer = document.querySelector("#city-list");
 var currentCityContainer = document.querySelector('#current-city-info')
 
+
 $("#search-city").click(function (event) {
     // console.log(event.target)
     latLong();
@@ -53,51 +54,56 @@ var citySearch = function (coord1, coord2) {
         })
 
         .then(function (data) {
-            console.log (data)
+            console.log(data)
             var temp = data.current.temp
             var wind = data.current.wind_speed
             var humidity = data.current.humidity
             var uvIndex = data.current.uvi
             currentData(temp, wind, humidity, uvIndex)
-            
+
 
         });
 
 }
 
 var appendCity = function (cityList) {
+    var currentInfo = document.createElement("h2")
     var cityLi = document.createElement("li");
     var cityButton = document.createElement("button")
-
-
+    var currentDateAndTime = (moment().format("MMMM D, YYYY"))
+    
+    
 
     cityButton.textContent = cityList
     cityButton.setAttribute("class", "button is-success my-cities");
+    currentInfo.setAttribute("class", "has-text-weight-bold is-size-3")
 
     cityContainer.append(cityLi)
     cityLi.append(cityButton)
-
-
+    currentCityContainer.append(currentInfo)
+    currentInfo.append(cityList +"  "+ currentDateAndTime)
+    
 }
 
 var currentData = function (currentInfo1, currentInfo2, currentInfo3, currentInfo4) {
-    
+
     var currentCityInfo1 = document.createElement("p")
     var currentCityInfo2 = document.createElement("p")
     var currentCityInfo3 = document.createElement("p")
     var currentCityInfo4 = document.createElement("p")
 
-   
+
     currentCityInfo1.textContent = "Temperature :  " + currentInfo1 + "  F"
     currentCityInfo2.textContent = "Wind Speed :  " + currentInfo2 + "  MPH"
     currentCityInfo3.textContent = "Humidity :  " + currentInfo3 + "  %"
     currentCityInfo4.textContent = "UVI :  " + currentInfo4
 
 
-    
+
     currentCityContainer.append(currentCityInfo1)
     currentCityContainer.append(currentCityInfo2)
     currentCityContainer.append(currentCityInfo3)
     currentCityContainer.append(currentCityInfo4)
+
 
 }
