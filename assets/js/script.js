25ef963b498f64e5fce9b278e0d9d963980bae6f
ryanmuhl <ryanmuhl@hotmail.com>
2022-04-02 10:14:10 -0400

    update 5 day forcast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 
 var cityContainer = document.querySelector("#city-list");
 var currentCityContainer = document.querySelector('#current-city-info')
+var fiveDayWeather = document.querySelector('#five-day1')
 
 
 $("#search-city").click(function (event) {
+    event.preventDefault()
     // console.log(event.target)
     latLong();
 
@@ -29,14 +31,17 @@ var latLong = function () {
             citySearch(lati, longi)
 
             var currentCity = data.name
+            
 
-            appendCity(currentCity)
+            var currentDate = moment.unix(data.dt).format("MM/DD/YYYY")
+
+            appendCity(currentCity, currentDate)
 
         });
 
-
-
     }
+
+
 
 
 var citySearch = function (coord1, coord2) {
@@ -61,26 +66,16 @@ var citySearch = function (coord1, coord2) {
             var uvIndex = data.current.uvi
             currentData(temp, wind, humidity, uvIndex)
 
-            var dailyWeather = data.daily
-            for (i=0; i< 5; i++) {
-                var forcast = dailyWeather[i]
-                var weatherDataAll = forcast.temp
-                console.log(weatherDataAll)
-                fiveDayForcast()
-            }
-            
+            renderForcast (data.daily);
         });
 
 }
 
-var appendCity = function (cityList) {
+var appendCity = function (cityList, curDate) {
     var currentInfo = document.createElement("h2")
     var cityLi = document.createElement("li");
     var cityButton = document.createElement("button")
-    var currentDateAndTime = (moment().format("MMMM D, YYYY"))
     
-    
-
     cityButton.textContent = cityList
     cityButton.setAttribute("class", "button is-success my-cities");
     currentInfo.setAttribute("class", "has-text-weight-bold is-size-3")
@@ -88,10 +83,12 @@ var appendCity = function (cityList) {
     cityContainer.append(cityLi)
     cityLi.append(cityButton)
     currentCityContainer.append(currentInfo)
-    currentInfo.append(cityList +"  "+ currentDateAndTime)
-    
+    currentInfo.append(cityList)
+    currentInfo.append("   ")
+    currentInfo.append(curDate)
 }
 
+    
 var currentData = function (currentInfo1, currentInfo2, currentInfo3, currentInfo4) {
 
     var currentCityInfo1 = document.createElement("p")
@@ -99,22 +96,85 @@ var currentData = function (currentInfo1, currentInfo2, currentInfo3, currentInf
     var currentCityInfo3 = document.createElement("p")
     var currentCityInfo4 = document.createElement("p")
 
-
-    currentCityInfo1.textContent = "Temperature :  " + currentInfo1 + "  F"
+     currentCityInfo1.textContent = "Temperature :  " + currentInfo1 + "  F"
     currentCityInfo2.textContent = "Wind Speed :  " + currentInfo2 + "  MPH"
     currentCityInfo3.textContent = "Humidity :  " + currentInfo3 + "  %"
     currentCityInfo4.textContent = "UVI :  " + currentInfo4
-
-
 
     currentCityContainer.append(currentCityInfo1)
     currentCityContainer.append(currentCityInfo2)
     currentCityContainer.append(currentCityInfo3)
     currentCityContainer.append(currentCityInfo4)
-
-
 }
 
-var fiveDayForcast = function () {
+var renderForcast = function (data) {
+
+    for (let index = 0; index < 5; index++) {
+        
+        renderForcastCard (data[index]);
+        
+    }
+}
     
+    
+var renderForcastCard = function (data) {
+    console.log(data)
+
+    var date = document.createElement("h3")
+    var tempElement = document.createElement("p")
+    var windElement = document.createElement("p")
+    var humidityElement = document.createElement("p")
+    var uviElement = document.createElement("p")
+    var fiveDayDiv = document.createElement("div")
+
+    fiveDayDiv.setAttribute("class","five-day has-background-link weather-style has-text-white")
+
+
+    date.textContent = moment.unix(data.dt).format("MM/DD/YYYY")
+    tempElement.textContent = "Temp :  " + data.temp.day + "  F"
+    windElement.textContent = "Wind Speed :  " + data.wind_speed + "  MPH"
+    humidityElement.textContent = "Humidity :  " + data.humidity + "  %"
+    uviElement.textContent = "uvi :  " + data.uvi
+    
+    fiveDayDiv.append(date)
+    fiveDayDiv.append(tempElement)
+    fiveDayDiv.append(windElement)
+    fiveDayDiv.append(humidityElement)
+    fiveDayDiv.append(uviElement)
+  
+    document.getElementById("five-day-container").append(fiveDayDiv)
+
 }
+            
+            
+
+    
+
+
+
+
+    
+    
+    
+    
+
+
+
+
+// var fiveDayForcast = function (weather1, weather2, weather3, weather4) {
+//     var weatherInfo1 = document.createElement("p")
+//     var weatherInfo2 = document.createElement("p")
+//     var weatherInfo3 = document.createElement("p")
+//     var weatherInfo4 = document.createElement("p")
+
+//     weatherInfo1.textContent = "Temp  :  " + weather1 + " F"
+//     weatherInfo2.textContent = "Wind  :  " + weather2 + " MPH"
+//     weatherInfo3.textContent = "Humidity  :  " + weather3 + " %"
+//     weatherInfo4.textContent = "UVI  :  " + weather4
+
+//     fiveDayWeather.append (weatherInfo1)
+//     fiveDayWeather.append (weatherInfo2)
+//     fiveDayWeather.append (+ weatherInfo3)
+//     fiveDayWeather.append (weatherInfo4)
+// }
+    
